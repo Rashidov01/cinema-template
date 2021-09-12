824ea8cebf675b2d58222b73bc8921ecd0219367
Abdulloh <72493727+Rashidov01@users.noreply.github.com>
2021-09-12 21:59:19 +0500

    all done

--- js/lesson.js
+++ js/lesson.js
@@ -1,35 +1,49 @@
-//  Travel agency
+// var userName = 'bek';
+// var result = '';
 
-// var user = prompt('please enter number');
+// for (var i = userName.length - 1; i >= 0; i--){
+//   result = result + userName[i];
+// }
+// console.log(result);
+// var fizzBuzz = [];
+// var fizz = [];
+// var buzz = [];
+// var oddNumber = [];
 
-// if (user >= 800){
-//   console.log('you can travel USA');
-// } else if(user >= 600){
-//   console.log('dubai');
-// } else if (user >= 400){
-//   console.log('Turkey');
-// } else{
-//   console.log('Chorvoq sizga eng yaxsh variant');
+// for (var i = 0; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     fizzBuzz.push(i)
+//   } else if (i % 3 === 0) {
+//     fizz.push(i);
+//   } else if (i % 5 === 0) {
+//     buzz.push(i)
+//   } else{
+//     oddNumber.push(i);
+//   }
 // };
 
+// console.log(fizzBuzz);
+// console.log(fizz);
+// console.log(buzz);
+// console.log(`bu son lar bolinmayd hech qaysiga ${oddNumber}`);
 
-//1000 kotta yoki kichik
 
-// var userNumber = prompt('iltimos 1000 gacha bogan son kiriting');
+var elForm = document.querySelector('.main-form');
+var elInput = elForm.querySelector('.main-input');
+var elResult = document.querySelector('.result');
+var elBtn = document.querySelector('.output-btn');
+elForm.addEventListener('submit', function(e){
+  e.preventDefault();
 
-// if (userNumber === 1000){
-//   console.log('siz kiritgan son 1000');
-// } else if(userNumber > 1000) {
-//   console.log('siz kiritgan son 1000 dan kotta');
-// } else if(userNumber < 1000){
-//   console.log('siz kiritgan son 1000 dan kichik');
-// };
-
-// user name and surname
-
-var userName = prompt('please enter your name');
-var userSurname = prompt('please enter your surname');
-var userResult = '';
-
-userResult = userName+ ' ' + userSurname;
-console.log(userResult);
+  for(var i = 0; i <= 100; i++){
+    if(elInput.value % 3 === 0 && elInput.value % 5 === 0){
+      elResult.textContent = 'FizzBuzz';
+    } else if (elInput.value % 3 === 0){
+     elResult.textContent = 'Fizz';
+    } else if(elInput.value % 5 === 0){
+      elResult.textContent = 'Buzz';
+    } else{
+      elResult.textContent = `${elInput.value} (3 ga ham 5 ga ham bolinmaydi)`
+    };
+  }
+})
