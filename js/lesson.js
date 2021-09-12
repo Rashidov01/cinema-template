f14b087237619b6471f47ccda74d794009d2e57f
Abdulloh <72493727+Rashidov01@users.noreply.github.com>
2021-09-12 15:37:38 +0500

    all done

--- js/lesson.js
+++ js/lesson.js
@@ -1,4 +1,35 @@
-var number = prompt('please a number');
-number = prompt.value;
-console.log(number);
+//  Travel agency
 
+// var user = prompt('please enter number');
+
+// if (user >= 800){
+//   console.log('you can travel USA');
+// } else if(user >= 600){
+//   console.log('dubai');
+// } else if (user >= 400){
+//   console.log('Turkey');
+// } else{
+//   console.log('Chorvoq sizga eng yaxsh variant');
+// };
+
+
+//1000 kotta yoki kichik
+
+// var userNumber = prompt('iltimos 1000 gacha bogan son kiriting');
+
+// if (userNumber === 1000){
+//   console.log('siz kiritgan son 1000');
+// } else if(userNumber > 1000) {
+//   console.log('siz kiritgan son 1000 dan kotta');
+// } else if(userNumber < 1000){
+//   console.log('siz kiritgan son 1000 dan kichik');
+// };
+
+// user name and surname
+
+var userName = prompt('please enter your name');
+var userSurname = prompt('please enter your surname');
+var userResult = '';
+
+userResult = userName+ ' ' + userSurname;
+console.log(userResult);
